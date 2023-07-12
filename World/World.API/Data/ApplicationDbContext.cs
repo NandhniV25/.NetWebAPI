13cae4f2738a87357ee7db9b0743e8e091b1e80e
Nandhni G A <nandhni.25@gmail.com>
2023-07-12 22:48:30 +0700

    world-country

---
+++ World/World.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using World.API.Models;
+
+namespace World.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //constructor ctor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //create country table and configure
+        public DbSet<Country> Countries { get; set; }
+    }
+}
