f78e31418d6ee6434ca14b1363b7befa0ed12470
Nandhni G A <nandhni.25@gmail.com>
2023-09-04 18:16:12 +0700

    changing the edit and add option menu

--- NoteBook/NotesWeb/Pages/NotesByIdDelete.cshtml.cs
+++ NoteBook/NotesWeb/Pages/NotesByIdDelete.cshtml.cs
@@ -1,12 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NotesWeb.DTO;
 
 namespace NotesWeb.Pages
 {
     public class NotesByIdDeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public NotesByIdDeleteModel(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        [BindProperty]
+        public NotesDto Notes { get; set; }
+        public void OnGet(int id)
+        {
+            if (id == 0)
+            {
+                Notes = new NotesDto();
+            }
+            else
+            {
+                var httpClient = _httpClientFactory.CreateClient("NotesWebAPI");
+                Notes = httpClient.GetFromJsonAsync<NotesDto>($"api/Notes/{id}").Result;
+            }
+        }
+        public void OnPost()
+        {
+            var httpClient = _httpClientFactory.CreateClient("NotesWebAPI");
+
+            var result = httpClient.DeleteAsync($"api/Notes/{Notes.Id}").Result;
+            Response.Redirect("/Notes");
         }
     }
 }
